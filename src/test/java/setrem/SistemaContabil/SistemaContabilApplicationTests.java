3d5f9c2d35f8f92e5dc66415737c1a0e0ee80f82
Guilherme Schons <gschonswow@gmail.com>
2020-10-11 14:42:07 -0300

    Base do projeto

---
+++ src/test/java/setrem/SistemaContabil/SistemaContabilApplicationTests.java
@@ -0,0 +1,13 @@
+package setrem.SistemaContabil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaContabilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
